61698e0f107a25b39aea387d405a84121a863fed
Jimmy Sung <jimmy43333@gmail.com>
2017-10-18 11:25:14 +0800

    Initial Commit

---
+++ Segmentation_ImageAnalysis/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Segmentation_ImageAnalysis
+//
+//  Created by 宋題均 on 2017/10/18.
+//  Copyright © 2017年 宋題均. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
